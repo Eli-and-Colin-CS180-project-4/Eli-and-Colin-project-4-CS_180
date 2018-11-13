1e587e688d9d0853fdb09fdf1e5668bc8219c71f
Colin <42813166+Vinr1ch@users.noreply.github.com>
2018-11-12 19:47:28 -0500

    Update ChatMessage.java
    This is implementing the constructor and getter methods

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -3,7 +3,22 @@ import java.io.Serializable;
 
 final class ChatMessage implements Serializable {
     private static final long serialVersionUID = 6898543889087L;
+    int num;
+    String str;
+
 
     // Here is where you should implement the chat message object.
     // Variables, Constructors, Methods, etc.
+    public ChatMessage(int num, String str){
+        this.str = str;
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getStr() {
+        return str;
+    }
 }
