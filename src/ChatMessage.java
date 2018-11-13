ddcfd0532f8c780de7f8938045af457a4f66f783
eheminger <eheminger@MSI.localdomain>
2018-11-13 11:22:30 -0500

    commiting to update

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -3,7 +3,13 @@ import java.io.Serializable;
 
 final class ChatMessage implements Serializable {
     private static final long serialVersionUID = 6898543889087L;
+    private String message;
+    private int type;
 
     // Here is where you should implement the chat message object.
     // Variables, Constructors, Methods, etc.
+    public ChatMessage(int type, String message) {
+        this.message = message;
+        this.type = type;
+    }
 }
